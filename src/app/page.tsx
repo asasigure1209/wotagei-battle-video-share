87f36cccf2f2e8cff7c5ec13dff0ef979b2ca251
asasigure <mkmokymh@gmail.com>
2023-10-09 01:04:41 +0900

    add: form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>エルニーニョ Vol. 6</h1>
+      <form method="get" action="videos">
+        <input
+          type="email"
+          name="email"
+          placeholder="エントリー時のメールアドレス"
+        ></input>
+        <button type="submit">自分の動画を探す</button>
+      </form>
+    </main>
+  );
 }
